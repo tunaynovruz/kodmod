c5066be3076ce051f0a9b5bdabc636d83ae9396f
Tunay Novruz <tunaynovruz@gmail.com>
2025-09-14 22:14:55 +0300

    update: gtm added

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -43,6 +43,9 @@ const config = {
         theme: {
           customCss: './src/css/custom.css',
         },
+        googleTagManager: {
+          containerId: 'GTM-NK8FH8HZ',
+        },
       }),
     ],
   ],
